0d79f644a059fa4121f0c238e679eb304bc9419e
almathaler <athaler00@stuy.edu>
2018-12-14 14:47:58 -0500

    remember to check add/remove for adding to beginning

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,6 +1,10 @@
 import java.util.NoSuchElementException;
 import java.lang.IndexOutOfBoundsException;
 public class MyLinkedList{
+
+
+
+
  private int size;
  private Node start,end;
 
@@ -27,9 +31,11 @@ public class MyLinkedList{
      System.out.println("Current size: " + list.size());
      list.remove(1);
      System.out.println("Removed 65: " + list.toString());
-     Integer intObj1 = new Integer(34);
+     Integer intObj1 = Integer.valueOf(34);
      list.remove(intObj1);
      System.out.println("Removed 34 using remove by type: " + list.toString());
+     list.clear();
+     System.out.println("Cleared the list. What does it look like?: " + list.toString());
    }catch(IndexOutOfBoundsException e){
      System.out.println(e);
    }
@@ -41,83 +47,93 @@ public class MyLinkedList{
    end = null;
  }
 
- public void clear(){
+ public void clear(){ //just make it no longer point to start and end
     start = null;
     end = null;
+    size = 0;
  }
  public int size(){
    return size;
  }
 
  public boolean add(Integer value){
-   Node toAdd = new Node(value, null, null);
-   if (start == null && end == null){
-     start = toAdd;
+   Node toAdd = new Node(value, null, null); //make a new node with the value we want, but don't yet link it to list
+   if (start == null && end == null){ //if you're adding to an empty list, make toAdd both start and end
+     start = toAdd; //but don't make toAdd point to itself as next and prev, that's bad
      end = toAdd;
    }
-   else{
-     end.setNext(toAdd);
-     toAdd.setNext(null);
-     toAdd.setPrev(end);
-     end = toAdd;
+   else{ //however we will add a next to the hypothetical only element here where we:
+     end.setNext(toAdd); //make whatever is the current end point to Node we are adding
+     //toAdd.setNext(null); //make toAdd's next null(kinda irrelevant because it was already null -- test if this can be removed) -- seems like it can be
+     toAdd.setPrev(end); //make whatever was added point back to what was before the end
+     end = toAdd; //give the title of end to the node that was added
    }
-   size+=1;
-   return true;
+   size+=1; //don't forget to increase size
+   return true; //since we did it, true
  }
 
  private Node getNthNode(int nth) throws IndexOutOfBoundsException{
-   if (nth >= size || nth < 0){
+   if (nth >= size || nth < 0){ //if nth isn't an actual index, give exception
      throw new IndexOutOfBoundsException("requested index out of bounds: " + nth);
    }
-   Node current = start;
-   for (int currentIndex = 0; currentIndex < nth; currentIndex++){
-     current = current.next();
+   Node current = start; //starting from the start (0th index) node;
+   for (int currentIndex = 0; currentIndex < nth; currentIndex++){ //cycle through n times, starting from 0th, setting current to one after current
+     current = current.next(); //it's appropriate to make <n and not <=n cuz this block set's current to whatever's after, what's after the (n-1)th index is n
    }
-   return current;
+   return current; //this method is LINEAR
  }
 
  public Integer get(int i) throws IndexOutOfBoundsException{
-   if (i >= size || i < 0){
+   if (i >= size || i < 0){ //the only reason we through exception here and not in getNthIndex is for clarity
      throw new IndexOutOfBoundsException("GETrequested index out of bounds: " + i);
    }
-   return getNthNode(i).getData();
+   return getNthNode(i).getData(); //use getNthNode and then return value -- user doesn't need to deal with node object
  }
 
  public Integer set(int i, Integer value) throws IndexOutOfBoundsException{
-   if (i >= size || i < 0){
+   if (i >= size || i < 0){ //same explanation for get
      throw new IndexOutOfBoundsException("SETrequested index out of bounds: " + i);
    }
-   Integer toReturn = this.get(i);
-   getNthNode(i).setData(value);
-   return toReturn;
+   Integer toReturn = this.get(i); //we return the old value, getting old value is 0(N)
+   getNthNode(i).setData(value); //another linear pass
+   return toReturn; //return old value
  }
 
  public boolean contains(Integer value){
-   for (int i = 0; i<size; i++){
-     if (this.get(i).equals(value)){
-       return true;
+   Node current = start;
+   for (int i = 0; i<size; i++){ //go through entire list
+     if (current.getData().equals(value)){ //NOT GOOD THIS METHOD IS QUADRATIC!!! NO -- MAKE IT LINEAR
+       return true; //made it linear by not using the get() method but by using currnet = start, current = current.next() instead of repeated gets
      }
+     current = current.next();
    }
    return false;
  }
 
- public int indexOf(Integer value){
+ public int indexOf(Integer value){ //AGAIN THIS IS QUADRATIC MAKE IT LINEAR BY USING CURRENT VARIABLE
+   Node current = start;
    for (int i = 0; i<size; i++){
-     if (this.get(i).equals(value)){
+     if (current.getData().equals(value)){
        return i;
      }
+     current = current.next();
    }
    return -1;
  }
-
+//remember, you don't have to declare new Integers you just have to do Integer.valueOf(#youwant)
  public void add(int index,Integer value){
-   if (index >= size || index<0){
+   if (index > size || index<0){ //we can add to size index, that's just adding to the end
      throw new IndexOutOfBoundsException("ADDrequested index out of bounds: " + index);
    }
-   Node toAdd = new Node(value, this.getNthNode(index), this.getNthNode(index-1));
-   this.getNthNode(index).setPrev(toAdd);
-   this.getNthNode(index-1).setNext(toAdd);
-   size+=1;
+   if (index == size){
+     this.add(value);
+   }
+   else{ //check, adding to the beginning of the list will make error since index-1 is outofbounds
+     Node toAdd = new Node(value, this.getNthNode(index), this.getNthNode(index-1));
+     this.getNthNode(index).setPrev(toAdd);
+     this.getNthNode(index-1).setNext(toAdd);
+     size+=1;
+   }
  }
 
  public Integer remove(int index) throws IndexOutOfBoundsException{
